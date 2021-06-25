5c3aebefc7b16c16da6eb24e5f63d879cf36f099
Steven Chia <stevenchia56@gmail.com>
2021-06-25 23:39:28 +0800

    add function to get walking distance based on lat long

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ PAGES = {
 st.sidebar.title('Singapore Private Property Price Prediction')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
+
 page.app()
--- price_prediction.py
+++ price_prediction.py
@@ -1,13 +1,30 @@
 import streamlit as st
 import requests
 import json
+import pandas as pd
+from math import sin, cos, sqrt, atan2, radians
+
+EMAIL = st.secrets["EMAIL"]
+PASSWORD = st.secrets["PASSWORD"]
 
 st.set_page_config(
     page_title="Singapore Private Property Price Prediction",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded"
-    )
+)
+
+
+def get_token(EMAIL, PASSWORD):
+    data = {
+        'email': EMAIL,
+        'password': PASSWORD
+    }
+
+    res = requests.post(
+        'https://developers.onemap.sg/privateapi/auth/post/getToken', json=data)
+
+    return res.json()['access_token']
 
 
 def get_lat_long(address):
@@ -19,37 +36,93 @@ def get_lat_long(address):
     elif data['found'] > 0:
         results = data['results'][0]
         latitude, longitude = results['LATITUDE'], results['LONGITUDE']
-        return latitude, longitude
+        return float(latitude), float(longitude)
+
+
+def get_distance(start_lat, end_lat, start_long, end_long):
+    R = 6373.0
+
+    start_lat = radians(start_lat)
+    end_lat = radians(end_lat)
+    start_long = radians(start_long)
+    end_long = radians(end_long)
+
+    dlon = end_long - start_long
+    dlat = end_lat - start_lat
+
+    a = sin(dlat / 2)**2 + cos(start_lat) * cos(end_lat) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
+
+
+def get_walking_distance(start_lat, start_long):
+    read_mrt_lat_long = pd.read_csv('mrtsg.csv')
+    distance = 100000000000000000000000000000000
+    selected_lat = 0
+    selected_long = 0
+
+    for mrt in range(len(read_mrt_lat_long)):
+        end_lat = read_mrt_lat_long['Latitude'].iloc[mrt]
+        end_long = read_mrt_lat_long['Longitude'].iloc[mrt]
+
+        current_distance = get_distance(
+            start_lat, end_lat, start_long, end_long)
+
+        if current_distance < distance:
+            distance = current_distance
+            selected_lat = end_lat
+            selected_long = end_long
+
+    TOKEN = get_token(EMAIL, PASSWORD)
+    url = "https://developers.onemap.sg/privateapi/routingsvc/route?" + \
+        "start=" + str(start_lat) + "," + str(start_long) + \
+        "&end=" + str(end_lat) + "," + str(end_long) + \
+        "&routeType=walk" + '&token=' + TOKEN
+    response = requests.get(url)
+    data = response.json()
+    if 'error' not in data.keys():
+        walking_distance = data['route_summary']['total_distance']
+    else:
+        walking_distance = 0
+
+    return walking_distance
 
 
 def app():
-    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+    st.write(
+        '<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
     with st.form('Form1'):
-        cols = st.beta_columns((2,1))
+        cols = st.beta_columns((2, 1))
         address = cols[0].text_input(label='Address', value='6 Shunfu Road')
         unit_num = cols[1].text_input(label='Unit number', value='#01-01')
 
         cols = st.beta_columns(3)
-        property_type = cols[0].selectbox('Property Type', ('Apartment/ Condo', 'Executive Condo', 'Other Landed Properties'))
-        sale_type = cols[1].selectbox('Type of Sale', ('New Sale', 'Resale', 'Sub Sale'))
-        region = cols[2].selectbox('Region', ('North', 'North East', 'East', 'West', 'Central'))
-        
-        area_sqft = st.number_input('Area SQFT')
-        
+        property_type = cols[0].selectbox(
+            'Property Type', ('Apartment/ Condo', 'Executive Condo', 'Other Landed Properties'))
+        sale_type = cols[1].selectbox(
+            'Type of Sale', ('New Sale', 'Resale', 'Sub Sale'))
+        region = cols[2].selectbox(
+            'Region', ('North', 'North East', 'East', 'West', 'Central'))
 
-        cols = st.beta_columns((1,2))
-        freehold = cols[0].radio('Freehold', ('Yes','No'))
-        tenure = cols[1].number_input('Remaining Tenure (If not freehold)', format ='%d', step=1)
+        area_sqft = st.number_input('Area SQFT')
+
+        cols = st.beta_columns((1, 2))
+        freehold = cols[0].radio('Freehold', ('Yes', 'No'))
+        tenure = cols[1].number_input(
+            'Remaining Tenure (If not freehold)', format='%d', step=1)
 
         submit_button = st.form_submit_button(label='Submit')
-        if submit_button:
-            latitude, longitude = get_lat_long(address) 
-            # need to calculate walking distance to nearest MRT
 
-            if len(unit_num) > 0:
-                floor_lvl = float(unit_num.split("-")[0][1:].lstrip("0")) # unit #01-01 gives you level 1
-                
-                
-            
-       
+    if submit_button:
+        latitude, longitude = get_lat_long(address)
+        walking_distance = get_walking_distance(latitude, longitude)
+
+        if len(unit_num) > 0:
+            # unit #01-01 gives you level 1
+            floor_lvl = float(unit_num.split("-")[0][1:].lstrip("0"))
+
+        st.write(walking_distance)
