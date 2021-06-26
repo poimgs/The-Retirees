e391a5e0a503c0f2d37e74f62b7c36d516f63e85
tiffanyemiliaa <tewijaya.2018@smu.edu.sg>
2021-06-26 17:43:25 +0800

    upload

--- dashboard.py
+++ dashboard.py
@@ -6,14 +6,10 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
-data_url = ("The-Retirees/singapore_housing_prices_750.xlsx")
-def load_data():
-    df = pd.read_excel(data_url)
-    return df
-
 #Title of dashboard
 st.title("Singapore Private Property Dashboard")
-st.markdown("This dashbaord will visualize the current landscaape of privaate properties in Singapore")
+st.markdown("This dashbaord will visualize the current landscaape of private properties in Singapore")
 
 #Let's show the dataset
+df = pd.read_csv(singapore_housing_prices_750.csv)
 st.write(df)
